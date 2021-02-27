fixed numerical instability problems

test.py
@@ -25,6 +25,7 @@ def test_model(
 
     print('\Computing evaluation metrics...')
     total_pixels = 0
+    total_examples = 0
     squared_error = 0
     rel_error = 0
     log_error = 0
@@ -46,15 +47,18 @@ def test_model(
 
             # TODO: Process y_pred in the optimal way (round it off, etc)
             # Maybe clamp from 0 to infty or something
-
+            nanmask = getNanMask(y_batch)
+            total_pixels = torch.sum(~nanmask)
+            total_examples += x_batch.shape[0]
+            
             # RMS, REL, LOG10, threshold calculation
-            squared_error += torch.sum(torch.pow(y_pred - y_batch, 2)).item()
-            rel_error += torch.sum(torch.abs(y_pred - y_batch) / y_batch).item()
-            log_error += torch.sum(torch.abs(torch.log10(y_pred + eps) - torch.log10(y_batch + eps))).item()
-            threshold1 += torch.sum(torch.max(y_pred / y_batch, y_batch / y_pred) < 1.25).item()
-            threshold2 += torch.sum(torch.max(y_pred / y_batch, y_batch / y_pred) < 1.25**2).item()
-            threshold3 += torch.sum(torch.max(y_pred / y_batch, y_batch / y_pred) < 1.25**3).item()
-            total_pixels += np.prod(y_batch.shape)
+            squared_error += (torch.sum(torch.pow(y_pred - y_batch, 2)).item() / total_pixels)**0.5
+            rel_error += torch.sum(torch.abs(y_pred - y_batch) / y_batch).item() / total_pixels
+            log_error += torch.sum(torch.abs(removeNans(torch.log10(y_pred)) - removeNans(torch.log10(y_batch)))).item() / total_pixels
+            threshold1 += torch.sum(torch.max(y_pred / y_batch, y_batch / y_pred) < 1.25).item() / total_pixels
+            threshold2 += torch.sum(torch.max(y_pred / y_batch, y_batch / y_pred) < 1.25**2).item() / total_pixels
+            threshold3 += torch.sum(torch.max(y_pred / y_batch, y_batch / y_pred) < 1.25**3).item() / total_pixels
+            # total_pixels += np.prod(y_batch.shape)
 
             progress_bar.update(len(x_batch))
 
@@ -65,16 +69,25 @@ def test_model(
     print('\n  Calculating overall metrics...')
     print()
     print('*' * 30)
-    print(f'RMS:   {(squared_error / total_pixels)**0.5}')
-    print(f'REL:   {rel_error / total_pixels}')
-    print(f'LOG10: {log_error / total_pixels}')
-    print(f'delta1:{threshold1 / total_pixels}')
-    print(f'delta2:{threshold2 / total_pixels}')
-    print(f'delta3:{threshold3 / total_pixels}')
+    print(f'RMS:   {squared_error / total_examples}')
+    print(f'REL:   {rel_error / total_examples}')
+    print(f'LOG10: {log_error / total_examples}')
+    print(f'delta1:{threshold1 / total_examples}')
+    print(f'delta2:{threshold2 / total_examples}')
+    print(f'delta3:{threshold3 / total_examples}')
     print('*' * 30)
 
     return model
 
+def removeNans(mat: torch.Tensor) -> torch.Tensor:
+    nanmask = torch.logical_or(torch.isnan(mat), torch.isinf(mat))
+    _mat = mat.clone()
+    _mat[nanmask] = 0
+    return _mat
+
+def getNanMask(mat: torch.Tensor) -> torch.Tensor:
+    nanmask = torch.logical_or(torch.isnan(mat), torch.isinf(mat))
+    return nanmask
 
 def main():
     parser = argparse.ArgumentParser()